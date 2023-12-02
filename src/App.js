d2fd54aabc2dd3bc77b3a3476694ac3e1173f594
Rick <rschloem@hs-mittweida.de>
2023-12-02 03:46:58 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, {} from 'react';
 import './Project.css';
 
 function App() {
 
    var pos = document.documentElement;
    pos.addEventListener('mousemove', e =>{
-       pos.style.setProperty('--x', e.clientX + 'px')
-       pos.style.setProperty('--y', e.clientY + 'px')
+       pos.style.setProperty('x', e.clientX + 'px')
+       pos.style.setProperty('y', e.clientY + 'px')
    })
 
     return (
