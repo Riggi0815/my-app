c8863095aa70bb4f89364bf7c7d87e8f728d4677
Rick <rschloem@hs-mittweida.de>
2023-12-16 14:03:28 +0100

    react-scroll/item

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,23 @@
 import React from 'react';
+//import icons 
+import { BiHomeAlt, BiUser } from 'react-icons/bi';
+import {BsClipboardData, BsBriefcase, BsChatSquare} from 'react-icons/bs';
+//link
+import {Link} from 'react-scroll';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return(
+    <nav className='fixed bottom-2'>
+      <div className='container mx-auto'>
+        {/*nav inner*/}
+        <div>
+          <Link>
+            <BiHomeAlt/>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
 };
 
 export default Nav;
