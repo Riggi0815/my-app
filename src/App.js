efca98668f1f8b92470f289bb2bf97c0827e8fb3
Rick <rschloem@hs-mittweida.de>
2024-02-02 23:33:43 +0100

    Portfolio change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 // components
-import Banner from './components/Banner';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import About from './components/About';
-import Services from './components/Services';
-import Work from './components/Work';
-import Contact from './components/Contact';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+import Testemonial from './components/Testemonial/Testemonial';
+import Footer from './components/Footer/Footer';
+import Services from './components/Services/Services';
 
 const App = () => {
-  return (
-    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
-      <Header />
-      <Banner />
-      <Nav />
-      <About />
-      <Services />
-      <Work />
-      <Contact />
-      <div className='h-[4000px]'></div>
-    </div>
-  );
+    return (
+        <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Portfolio />
+            <Contact />
+            <Testemonial />
+            <Footer />
+            <div className='h-[4000px]'></div>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/Testemonial/Testemonial.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './testemonial.css';
+
+const Testemonial = () => {
+    return (
+        <div>
+            Testemonial
+        </div>
+    );
+};
+
+export default Testemonial;
