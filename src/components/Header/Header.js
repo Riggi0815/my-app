16d25c37acde685943cc9afb7a3cc59d61914fce
Rick <rschloem@hs-mittweida.de>
2024-02-04 00:02:46 +0100

    Header first setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png'
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header>
+            <div className="container header_container">
+                <h5>Hello I'm</h5>
+                <h1>Rick Schlömilch</h1>
+                <h5 className="text-light">Developer</h5>
+                <CTA />
+
+                <div className="me">
+                    <img src={ME} alt="me"/>
+                </div>
+
+                <a href="#contact" className='scroll_down'>Scroll Down</a>
+            </div>
+        </header>
     );
 };
 
