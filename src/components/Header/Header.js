6d4c176b998221c197932a7d3f0b5092330f8df5
Rick Schlömilch <rschloem@hs-mittweida.de>
2024-02-08 22:54:45 +0100

    linked in link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './header.css';
 import CTA from './CTA';
 import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
     return (
@@ -11,6 +12,7 @@ const Header = () => {
                 <h1>Rick Schlömilch</h1>
                 <h5 className="text-light">Developer</h5>
                 <CTA />
+                <HeaderSocial />
 
                 <div className="me">
                     <img src={ME} alt="me"/>
--- src/components/Header/HeaderSocials.js
+++ src/components/Header/HeaderSocials.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { FaLinkedin } from "react-icons/fa";
 
 const HeaderSocials = () => {
   return(
       <div className='header_socials'>
-          <a href="https://linkedin.com" target="_blank"></a>
+          <a href="https://linkedin.com" target="_blank"> <FaLinkedin /> </a>
           <a href="https://github.com" target="_blank"></a>
       </div>
   )
-}
+};
+
+export default HeaderSocials
