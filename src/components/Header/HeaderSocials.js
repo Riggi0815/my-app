1d7ca394bebd9c013287538ad8c65546d1f3aa32
Rick Schlömilch <rschloem@hs-mittweida.de>
2024-02-14 11:45:57 +0100

    Header css

--- src/components/Header/HeaderSocials.js
+++ src/components/Header/HeaderSocials.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { FaLinkedin } from "react-icons/fa";
+import {FaGithub, FaLinkedin} from "react-icons/fa";
 
 const HeaderSocials = () => {
   return(
       <div className='header_socials'>
           <a href="https://linkedin.com" target="_blank"> <FaLinkedin /> </a>
-          <a href="https://github.com" target="_blank"></a>
+          <a href="https://github.com" target="_blank"><FaGithub /></a>
       </div>
   )
 };
